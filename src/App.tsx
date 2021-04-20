88195f54a3abf35e2af64531b9cdf4d3ed7363fd
MichaelRyanGoad <michael.ryan.goad@gmail.com>
2021-04-20 13:59:24 -0400

    Experience the Cube

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ThreeExample from "./Three/threeExample";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>EXPERIENCE THE PRISM</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>EXPERIENCE THE Cube</p>
+        <ThreeExample />
       </header>
     </div>
   );
